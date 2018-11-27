dcd03306bfdb00a78098fc8430ceb16b35637ae8
edipomrp <42787201+edipomrp@users.noreply.github.com>
2018-11-27 21:49:11 -0200

    Add files via upload

---
+++ TrabalhoOrdenacao/TrabalhoOrdenacao/OrdenacaoUtil.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrabalhoOrdenacao
+{
+    static class OrdenacaoUtil
+    {
+        public static int[] insertionSort(int[] vetor) {
+            int i, j, atual;
+            for (i = 1; i < vetor.Length; i++) {
+                atual = vetor[i];
+                j = i;
+                while ((j > 0) && (vetor[j - 1] > atual)) {
+                    vetor[j] = vetor[j - 1];
+                    j = j - 1;
+                }
+                vetor[j] = atual;
+            }
+
+            return vetor;
+        }
+
+        public static int[] selectionSort(int[] vetor) {
+            int min, aux;
+            for (int i = 0; i < vetor.Length - 1; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < vetor.Length; j++)
+                    if (vetor[j] < vetor[min])
+                        min = j;
+                if (min != i)
+                {
+                    aux = vetor[min];
+                    vetor[min] = vetor[i];
+                    vetor[i] = aux;
+                }
+            }
+            return vetor;
+        }
+
+        public static int[] bubbleSort(int[] vetor) {
+            int tamanho = vetor.Length;
+            int comparacoes = 0;
+            int trocas = 0;
+            for (int i = tamanho - 1; i >= 1; i--) {
+                for (int j = 0; j < i; j++) {
+                    comparacoes++;
+                    if (vetor[j] > vetor[j + 1]) {
+                        int aux = vetor[j];
+                        vetor[j] = vetor[j + 1];
+                        vetor[j + 1] = aux;
+                        trocas++;
+                    }
+                }
+            }
+            return vetor;
+        }
+
+        public static int[] quickSort(int[] vetor) {
+            int inicio = 0;
+            int fim = vetor.Length - 1;
+            quickSort(vetor, inicio, fim);
+
+            return vetor;
+
+        }
+
+
+        private static void quickSort(int[] vetor, int inicio, int fim)
+        {
+
+            if (inicio < fim)
+
+            {
+
+                int p = vetor[inicio];
+                int i = inicio + 1;
+                int f = fim;
+
+                while (i <= f)
+
+                {
+
+                    if (vetor[i] <= p)
+
+                    {
+
+                        i++;
+
+                    }
+
+                    else if (p < vetor[f])
+
+                    {
+
+                        f--;
+
+                    }
+
+                   else
+                    {
+                       int troca = vetor[i];
+                        vetor[i] = vetor[f];
+                        vetor[f] = troca;
+                        i++;
+                        f--;
+
+                    }
+                }
+                vetor[inicio] = vetor[f];
+                vetor[f] = p;
+                quickSort(vetor, inicio, f - 1);
+                quickSort(vetor, f + 1, fim);
+
+            }
+        }
+
+        public static void mergeSortNumbers(int[] B, int iStart, int iEnd, int[] A)
+        {
+            if ((iEnd - iStart) < 2) return;
+
+            int iMiddle = (iStart + iEnd) / 2;
+            mergeSortNumbers(A, iStart, iMiddle, B);
+            mergeSortNumbers(A, iMiddle, iEnd, B);
+            Merge(B, iStart, iMiddle, iEnd, A);
+        }
+
+        private static void Merge(int[] A, int iStart, int iMiddle, int iEnd, int[] B)
+        {
+            int i = iStart;
+            int j = iMiddle;
+
+            for (int k = iStart; k < iEnd; k++)
+            {
+                if (i < iMiddle && (j >= iEnd || A[i] <= A[j]))
+                {
+                    B[k] = A[i++];
+                }
+                else
+                {
+                    B[k] = A[j++];
+                }
+            }
+        }
+    }
+
+
+
+
+}
---
+++ TrabalhoOrdenacao/TrabalhoOrdenacao/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TrabalhoOrdenacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            int[] vetor1 = new int[100000];
+            int[] vetor2 = new int[100000];
+            int[] vetor3 = new int[100000];
+            
+            int j = 0;
+            iniciaVetores(vetor1, vetor2, vetor3);
+
+            Console.WriteLine("############################## INSERTION SORT #####################################\n");
+            stopwatch.Start();
+            OrdenacaoUtil.insertionSort(vetor1);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução da função de ordenação do Insertion Sort: " + stopwatch.Elapsed);
+            stopwatch.Reset();
+            stopwatch.Start();
+            OrdenacaoUtil.insertionSort(vetor2);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução vetor 2 método insertion Sort: " + stopwatch.Elapsed);
+            stopwatch.Reset();
+            stopwatch.Start();
+            OrdenacaoUtil.insertionSort(vetor3);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução vetor 3 método insertion Sort: " + stopwatch.Elapsed);
+            iniciaVetores(vetor1, vetor2, vetor3);
+            Console.WriteLine("############################## SELECTION SORT #####################################\n");
+            stopwatch.Reset();
+            stopwatch.Start();
+            OrdenacaoUtil.selectionSort(vetor1);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução vetor 1 método selection Sort: " + stopwatch.Elapsed);
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            OrdenacaoUtil.selectionSort(vetor2);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução vetor 2 método selection Sort: " + stopwatch.Elapsed);
+
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            OrdenacaoUtil.selectionSort(vetor3);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução vetor 3 método selection Sort: " + stopwatch.Elapsed);
+
+            Console.WriteLine("############################## BUBBLE SORT #####################################\n");
+            iniciaVetores(vetor1, vetor2, vetor3);
+            stopwatch.Reset();
+            stopwatch.Start();
+            OrdenacaoUtil.bubbleSort(vetor1);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução vetor 1 método Bubble Sort: " + stopwatch.Elapsed);
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            OrdenacaoUtil.bubbleSort(vetor2);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução vetor 2 método Bubble Sort: " + stopwatch.Elapsed);
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            OrdenacaoUtil.bubbleSort(vetor3);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução vetor 3 método Bubble Sort: " + stopwatch.Elapsed);
+
+            Console.WriteLine("############################## QUICK SORT #####################################\n");
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            OrdenacaoUtil.quickSort(vetor1);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução vetor 1 método Bubble Sort: " + stopwatch.Elapsed);
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            OrdenacaoUtil.quickSort(vetor2);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução vetor 2 método Bubble Sort: " + stopwatch.Elapsed);
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            OrdenacaoUtil.quickSort(vetor3);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução vetor 3 método Bubble Sort: " + stopwatch.Elapsed);
+            
+
+            Console.WriteLine("############################## MERGE SORT #####################################\n");
+            stopwatch.Reset();
+            stopwatch.Start();
+            OrdenacaoUtil.mergeSortNumbers(new int[vetor1.Length], 0, vetor1.Length, vetor1);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução vetor 1 método Merge Sort: " + stopwatch.Elapsed);
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            OrdenacaoUtil.mergeSortNumbers(new int[vetor2.Length], 0, vetor2.Length, vetor2);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução vetor 2 método Merge Sort: " + stopwatch.Elapsed);
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            OrdenacaoUtil.mergeSortNumbers(new int[vetor3.Length], 0, vetor3.Length, vetor3);
+            stopwatch.Stop();
+            Console.WriteLine("Tempo de execução vetor 3 método Merge Sort: " + stopwatch.Elapsed);
+
+            Console.ReadLine();
+        }
+
+
+        public static void iniciaVetores(int[] vet1, int[] vet2, int[] vet3) {
+            int j = 0;
+            Random random = new Random();
+            for (int i = 0; i < 100000; i++)
+            {
+                vet1[i] = i;
+            }
+
+            for (int i = 100000; i > 0; i--)
+            {
+
+                vet2[j++] = i;
+            }
+
+            for (int i = 0; i < 100000; i++)
+            {
+
+                int numRandom = random.Next(0, 100000);
+                vet3[i] = numRandom;
+            }
+        }
+
+        
+    }
+}
